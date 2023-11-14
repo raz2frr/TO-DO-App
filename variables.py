0cb5474155d2f145f509ccc914f98463c86327cb
raz2frr <geo2greedy@gmail.com>
2023-11-14 16:55:41 -0500

    I am pushing my python and virgin control files

---
+++ variables.py
@@ -0,0 +1,49 @@
+from fileinput import close
+
+
+'app_name=To_Do_App'
+'(function) sub_process: any'
+'(function) To_Do_App: app_name'
+'sub_process.run' '[To_Do_App]'
+  #ask user for their age ans store in variables
+user_age=input ("Please enter you're age")
+user_age=int(user_age)
+print("you entered: {user_age} years old")
+  #if user age is below 10 display sorry message and shut down program
+if user_age<10:
+    print("sorry you're too young!")
+'else:'
+print("congradulation you're account has been created!")
+  #make a list of catergories
+  #print the list
+  #take input from user
+  #check if catergory matches those in the category list
+catergories=["school","work","health"]
+print(catergories)
+user_input=("school")
+'(function) quests: any'
+print('quests')
+quests=["do math homework","finish history project","practice spanish on family member"]
+input("do math homework")
+  #ask user if finished with quest
+input("quest completed?")
+  #check users answer
+If_user_answer=("yes")
+  #award points and notify user of completion
+award_point=("award points based on difficulty of quest")
+print("congradulation, you've earned (number of award points) award points!")
+'else:'
+    #no points earned message
+'Print'("no points earned try again later!")
+    #close app
+  #if user wants to continue doing quests
+'Print'("thank you for using this app, continue doing more quests?")
+  #if user continue quests
+input("continue doing quests")
+'else:'
+    #user closes app
+input ("close app")
+    #app closes
+
+
+
---
+++ whatifs.py
@@ -0,0 +1,25 @@
+age=-10
+#if age is under 10 close the app
+'if (function) user_age>10:'
+print("sorry you're still too young to be using this app!")
+'else:'
+print("account has been successfully made!")
+
+'quest=completed'
+'if user_answer=yes'
+award_point=('award point based on difficulty of quest')
+print("congrats, you've earned (number of points)award points!")
+'else:'
+#no points earned message
+print("no points earned, try again later!")
+#close app
+
+'continue_quest=yes'
+'if user_answer=yes'
+print("thank you for using this app, continue more quests?")
+#user continues quests
+input("continue doing quests")
+'else:'
+#user clolses app
+input ("close app")
+#app closes
